efbee634dd0dfdf1f4297313d87c36f6c9037b1c
tinashe2008 <162554125+tinashe2008@users.noreply.github.com>
2024-03-07 15:10:58 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "random" {
+  version = "~> 3.1"
+}
+
+resource "random_pet" "name" {
+  keepers = {
+    # Updating this value causes Terraform to generate a new name
+    uuid = "1"
+  }
+  length = 10
+  prefix = "cloud"
+  separator = "-"
+}
+
+
+output "pet_name" {
+  description = "The random pet name"
+  value       = random_pet.name.id
+}
